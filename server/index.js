ef53bc69c68b3174f7dd929e188b9ca0ffc2fe6b
wenjielee11 <leewenjie.wjl@gmail.com>
2023-11-12 04:51:40 -0600

    ??

--- server/index.js
+++ server/index.js
@@ -9,11 +9,10 @@ const multer = require("multer");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: "50mb" }));
-app.use(express.static(path.join(__dirname, 'build')));
 const port = process.env.PORT || 2023;
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/build/index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(`${process.cwd()}`+'/build/index.html'));
   });
   
 
